aa7b8b9f1dcf930384b930e323c548365f912eee
tlpranathi <tlakshmipranathi@gmail.com>
2026-07-06 15:55:41 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,8 @@ app.get("/", (req, res)=>{
 
 app.use(errorHandler)
 
-app.listen(5000, ()=>{
-    console.log("server running on port 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
