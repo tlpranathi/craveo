520acd516d8b4da1d15daf48d114a4fc71aec719
tlpranathi <tlakshmipranathi@gmail.com>
2026-07-06 16:04:18 +0530

    deploy: configure frontend for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 // create reusable axios instance
 // baseURL is automatically added to every request 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 // request interceptor
