e10603fc540a4549e419d04e082eaf9af9a554c9
tlpranathi <tlakshmipranathi@gmail.com>
2026-07-20 09:08:58 +0530

    fix email error

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -8,7 +8,7 @@ const { passwordChangedTemplate } = require("../templates/passwordChangedEmail")
 const { passwordResetTemplate } = require("../templates/passwordResetEmail");
 
 const transporter = nodemailer.createTransport({
-    service: "smtp.gmail.com",
+    host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
